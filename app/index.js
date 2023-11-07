7c47a9bf0695f057ece0a3689d5eeb63c49be3f8
cuongnm3110 <cuongnm3110@gmail.com>
2023-11-07 15:54:13 +0700

    first commit

---
+++ app/index.js
@@ -0,0 +1,66 @@
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  FlatList,
+  Linking,
+} from "react-native";
+import { Stack } from "expo-router";
+import StarRating from "react-native-star-rating-widget";
+
+import { data } from "../contants/data";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Stack.Screen
+        options={{
+          title: "Movie",
+        }}
+      />
+      <View style={{ padding: 16 }}>
+        <FlatList
+          data={data}
+          renderItem={({ item }) => (
+            <View style={{ width: 200 }}>
+              <TouchableOpacity
+                onPress={() => {
+                  Linking.openURL(item.image.uri);
+                }}
+              >
+                <Image
+                  source={item.image}
+                  style={{ height: 200, width: 200, borderRadius: 20 }}
+                />
+              </TouchableOpacity>
+              <Text style={{ opacity: 0.6, fontSize: 12, marginTop: 8 }}>
+                {item.category}
+              </Text>
+              <StarRating
+                rating={item.rating}
+                starSize={20}
+                style={{ marginTop: 8, marginBottom: 8 }}
+              />
+              <TouchableOpacity
+                onPress={() => {
+                  Linking.openURL(item.image.uri);
+                }}
+              >
+                <Text style={{ fontSize: 20, fontWeight: 500 }}>
+                  {item.title}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          )}
+          keyExtractor={(item) => item.id}
+          contentContainerStyle={{ columnGap: 16 }}
+          horizontal
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
---
+++ contants/data.js
@@ -0,0 +1,44 @@
+export const data = [
+  {
+    id: 1,
+    category: "SCIENCE FICTION",
+    title: "Blade Runner Future 2049",
+    image: { uri: "https://i.imgur.com/L68FtMA.jpg" },
+    rating: 4.0,
+  },
+  {
+    id: 2,
+    category: "SUPERHERO ACTION",
+    title: "Captain America Civil War.",
+    image: { uri: "https://i.imgur.com/tAui2H7.jpg" },
+    rating: 3.5,
+  },
+  {
+    id: 3,
+    category: "ROMANCE",
+    title: "Beauty and the Beast",
+    image: { uri: "https://i.imgur.com/J5pgFyr.jpg" },
+    rating: 2.5,
+  },
+  {
+    id: 4,
+    category: "DRAMA",
+    title: "Shawshank Redemption",
+    image: { uri: "https://i.imgur.com/7YGXDbM.jpg" },
+    rating: 3.0,
+  },
+  {
+    id: 5,
+    category: "SCIENCE FICTION",
+    title: "God Father",
+    image: { uri: "https://i.imgur.com/J5pgFyr.jpg" },
+    rating: 5,
+  },
+  {
+    id: 6,
+    category: "SUPERHERO ACTION",
+    title: "Thor",
+    image: { uri: "https://i.imgur.com/tAui2H7.jpg" },
+    rating: 0.5,
+  },
+];
